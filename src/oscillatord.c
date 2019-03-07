645137a99786a20d7690d4d6477b80217db58f5c
Nicolas Carrier <nicolas.carrier@orolia.com>
2019-03-07 18:38:11 +0100

    oscillator control implementation and config parsing

--- src/oscillatord.c
+++ src/oscillatord.c
@@ -17,6 +17,11 @@
 
 #include <oscillator-disciplining/oscillator-disciplining.h>
 
+#include "log.h"
+#include "oscillator.h"
+#include "oscillator_factory.h"
+#include "config.h"
+
 /*
  * The driver has a watchdog which resets the 1PPS device if no interrupt has
  * been received in the last two seconds, so a timeout of more than 4 seconds
@@ -46,16 +51,34 @@ int main (int argc, char *argv[])
 	ssize_t sret;
 	int32_t phase_error;
 	const char *device;
+	struct config config;
+	const char *path;
+	struct __attribute__((cleanup(oscillator_factory_destroy)))
+			oscillator *oscillator = NULL;
 
 	if (argc != 2)
 		error(EXIT_FAILURE, 0, "usage: %s pps_device_path", argv[0]);
+	path = argv[1];
 
-	device = argv[1];
+	ret = config_init(&config, path);
+	if (ret != 0)
+		error(EXIT_FAILURE, -ret, ERR "config_init(%s)", path);
+
+	device = config_get(&config, "pps-device");
+	if (device == NULL)
+		error(EXIT_FAILURE, errno, ERR "PPS device not defined in "
+				"config %s", path);
+	printf("PPS device %s\n", device);
+
+	oscillator = oscillator_factory_new(&config);
+	if (oscillator == NULL)
+		error(EXIT_FAILURE, errno, ERR "oscillator_factory_new");
+	printf("oscillator model %s\n", oscillator->factory_name);
+
 	fd = open(device, O_RDWR);
 	if (fd == -1)
 		error(EXIT_FAILURE, errno, "open(%s)", device);
 
-	printf("device is %s\n", device);
 	od = od_new(CLOCK_MONOTONIC);
 	if (od == NULL)
 		error(EXIT_FAILURE, errno, "od_new");
@@ -112,6 +135,10 @@ int main (int argc, char *argv[])
 				input.phase_error.tv_nsec,
 				input.valid ? "true" : "false");
 		printf("output: setpoint = %"PRIu32"\n", output.setpoint);
+		ret = oscillator_set_dac(oscillator, output.setpoint);
+		if (ret < 0)
+			error(EXIT_FAILURE, -ret, "oscillator_set_dac");
+
 	}
 
 	od_destroy(&od);
