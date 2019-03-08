7388810865af588add3564b160c7beace5b6c009
Nicolas Carrier <nicolas.carrier@orolia.com>
2019-03-08 09:36:01 +0100

    check arguments of oscillator methods

--- src/oscillator.c
+++ src/oscillator.c
@@ -4,21 +4,33 @@
 
 int oscillator_set_dac(struct oscillator *oscillator, unsigned value)
 {
+	if (oscillator == NULL)
+		return -EINVAL;
+
 	return oscillator->set_dac(oscillator, value);
 }
 
 int oscillator_get_dac(struct oscillator *oscillator, unsigned *value)
 {
+	if (oscillator == NULL || value == NULL)
+		return -EINVAL;
+
 	return oscillator->get_dac(oscillator, value);
 }
 
 int oscillator_save(struct oscillator *oscillator)
 {
+	if (oscillator == NULL)
+		return -EINVAL;
+
 	return oscillator->save(oscillator);
 }
 
 int oscillator_get_temp(struct oscillator *oscillator, uint16_t *temp)
 {
+	if (oscillator == NULL || temp == NULL)
+		return -EINVAL;
+
 	return oscillator->get_temp(oscillator, temp);
 }
 
