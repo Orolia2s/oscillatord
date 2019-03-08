10ee4c9e47d3396758848682c731288481682347
Nicolas Carrier <nicolas.carrier@orolia.com>
2019-03-08 09:35:19 +0100

    rakon oscillator support

--- src/oscillators/rakon.c
+++ src/oscillators/rakon.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <errno.h>
+#include <inttypes.h>
 
 #include <spi2c.h>
 
@@ -7,15 +8,41 @@
 #include "../oscillator_factory.h"
 #include "../config.h"
 #include "../log.h"
+#include "../utils.h"
 
 #define FACTORY_NAME "rakon"
+#define RAKON_CMD_SET_DAC 0xc0
+#define RAKON_CMD_GET_DAC 0xc1
+#define RAKON_CMD_SAVE 0xc2
+
+struct rakon_oscillator {
+	struct oscillator oscillator;
+	struct i2cdev *i2c;
+};
 
 static unsigned rakon_oscillator_index;
 
 static int rakon_oscillator_set_dac(struct oscillator *oscillator,
 		unsigned value)
 {
-	info("%s(%p, %u)\n", __func__, oscillator, value);
+	uint8_t buf[4];
+	struct rakon_oscillator *rakon;
+	int ret;
+
+	rakon = container_of(oscillator, struct rakon_oscillator, oscillator);
+
+	info("%s(%s, %u)\n", __func__, oscillator->name, value);
+
+	buf[0] = RAKON_CMD_SET_DAC;
+	buf[1] = (value & 0x0F0000) >> 16;
+	buf[2] = (value & 0xFF00) >> 8;
+	buf[3] = value & 0xFF;
+	ret = i2c_transfer(rakon->i2c, buf, sizeof(buf), NULL, 0);
+	if (ret != 0) {
+		ret = -errno;
+		err("failed i2c transfer : %m\n");
+		return ret;
+	}
 
 	return 0;
 }
@@ -23,14 +50,44 @@ static int rakon_oscillator_set_dac(struct oscillator *oscillator,
 static int rakon_oscillator_get_dac(struct oscillator *oscillator,
 		unsigned *value)
 {
-	info("%s(%p, %p)\n", __func__, oscillator, value);
+	uint8_t tx_val;
+	uint8_t buf[3];
+	struct rakon_oscillator *rakon;
+	int ret;
+
+	rakon = container_of(oscillator, struct rakon_oscillator, oscillator);
+
+	tx_val = RAKON_CMD_GET_DAC;
+	ret = i2c_transfer(rakon->i2c, &tx_val, sizeof(tx_val), buf,
+			sizeof(buf));
+	if (ret != 0) {
+		ret = -errno;
+		err("failed i2c transfer : %m\n");
+		return ret;
+	}
+
+	info("%s(%s) = %u\n", __func__, oscillator->name, *value);
 
 	return 0;
 }
 
 static int rakon_oscillator_save(struct oscillator *oscillator)
 {
-	info("%s(%p)\n", __func__, oscillator);
+	uint8_t tx_val;
+	struct rakon_oscillator *rakon;
+	int ret;
+
+	info("%s(%s)\n", __func__, oscillator->name);
+
+	rakon = container_of(oscillator, struct rakon_oscillator, oscillator);
+
+	tx_val = RAKON_CMD_SAVE;
+	ret = i2c_transfer(rakon->i2c, &tx_val, sizeof(tx_val), NULL, 0);
+	if (ret != 0) {
+		ret = -errno;
+		err("failed i2c transfer : %m\n");
+		return ret;
+	}
 
 	return 0;
 }
@@ -38,36 +95,88 @@ static int rakon_oscillator_save(struct oscillator *oscillator)
 static int rakon_oscillator_get_temp(struct oscillator *oscillator,
 		uint16_t *temp)
 {
-	info("%s(%p, %p)\n", __func__, oscillator, temp);
+	struct rakon_oscillator *rakon;
+	int ret;
+
+	rakon = container_of(oscillator, struct rakon_oscillator, oscillator);
+
+	ret = i2c_transfer(rakon->i2c, NULL, 0, (uint8_t *)temp, sizeof(*temp));
+	if (ret != 0) {
+		ret = -errno;
+		err("failed i2c transfer : %m\n");
+		return ret;
+	}
+
+	info("%s(%s) = %"PRIu16"\n", __func__, oscillator->name, *temp);
 
 	return 0;
 }
 
+static void rakon_oscillator_destroy(struct oscillator **oscillator)
+{
+	struct oscillator *o;
+	struct rakon_oscillator *r;
+
+	if (oscillator == NULL || *oscillator == NULL)
+		return;
+
+	o = *oscillator;
+	r = container_of(o, struct rakon_oscillator, oscillator);
+	i2c_delete(r->i2c);
+	memset(o, 0, sizeof(*o));
+	free(o);
+	*oscillator = NULL;
+}
+
 static struct oscillator *rakon_oscillator_new(const struct config *config)
 {
+	struct rakon_oscillator *rakon;
+	int ret;
+	uint8_t i2c_num;
+	uint8_t i2c_addr;
 	struct oscillator *oscillator;
 
-	oscillator = calloc(1, sizeof(*oscillator));
-	if (oscillator == NULL)
+	rakon = calloc(1, sizeof(*rakon));
+	if (rakon == NULL)
 		return NULL;
+	oscillator = &rakon->oscillator;
+
+	ret = config_get_uint8_t(config, "rakon-i2c-num");
+	if (ret < 0) {
+		err("rakon-i2c-num config key must be provided\n");
+		goto error;
+	}
+	i2c_num = ret;
+	ret = config_get_uint8_t(config, "rakon-i2c-addr");
+	if (ret < 0) {
+		err("rakon-i2c-addr config key must be provided\n");
+		goto error;
+	}
+	i2c_addr = ret;
+
+	rakon->i2c = i2c_new(i2c_num, i2c_addr);
+	if (rakon->i2c == NULL) {
+		ret = -errno;
+		err("i2c_new: %m\n");
+		goto error;
+	}
 
 	snprintf(oscillator->name, OSCILLATOR_NAME_LENGTH, FACTORY_NAME "-%d",
 			rakon_oscillator_index);
-	rakon_oscillator_index++;
 	oscillator->set_dac = rakon_oscillator_set_dac;
 	oscillator->get_dac = rakon_oscillator_get_dac;
 	oscillator->save = rakon_oscillator_save;
 	oscillator->get_temp = rakon_oscillator_get_temp;
 	oscillator->factory_name = FACTORY_NAME;
 
-	return oscillator;
-}
+	info("Instantiated " FACTORY_NAME " oscillator on i2c number %" PRIu8
+			" and i2c address %" PRIu8 "\n", i2c_num, i2c_addr);
 
-static void rakon_oscillator_destroy(struct oscillator **oscillator)
-{
-	memset(*oscillator, 0, sizeof(**oscillator));
-	free(*oscillator);
-	*oscillator = NULL;
+	return oscillator;
+error:
+	rakon_oscillator_destroy(&oscillator);
+	errno = -ret;
+	return NULL;
 }
 
 static const struct oscillator_factory rakon_oscillator_factory = {
