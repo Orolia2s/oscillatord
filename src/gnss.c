4afd4aa9c7b7141c61c397759972f8e74d82e545
Charles Parent <charles.parent@orolia2s.com>
2021-07-22 10:19:50 +0200

    Allow gnss antenna to be in short state

--- src/gnss.c
+++ src/gnss.c
@@ -329,7 +329,7 @@ static void * gnss_thread(void * p_data)
 				uint8_t msgId = UBX_MSGID(msg->data);
 				if (clsId == UBX_MON_CLSID && msgId == UBX_MON_RF_MSGID) {
 					gnss_get_antenna_data(session, msg);
-					session->valid = session->fix >= MODE_NO_FIX && session->antenna_status == ANT_STATUS_OK;
+					session->valid = session->fix >= MODE_NO_FIX && (session->antenna_status == ANT_STATUS_OK || session->antenna_status == ANT_STATUS_OPEN);
 					log_gnss_data(session);
 				}
 			}
