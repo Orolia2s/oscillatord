8f7e3ccca9fdb81343947cc713e595f24cf0e7ce
Charles Parent <charles.parent@orolia2s.com>
2022-04-15 15:48:42 +0200

    Pass ptp device to extts via cli

--- tests/extts_test.c
+++ tests/extts_test.c
@@ -13,9 +13,14 @@ int main(int argc, char * argv[])
 	int fd_clock;
 	int ret;
 
+	if (argc < 2) {
+		log_error("Please specify path to ptp device !");
+		return -1;
+	}
+
 	signal(SIGINT, intHandler);
 	log_set_level(LOG_INFO);
-	fd_clock = open("/dev/ptp0", O_RDWR);
+	fd_clock = open(argv[1], O_RDWR);
 
 	for (int i = 0; i < NUM_EXTTS; i++) {
 		ret = enable_extts(fd_clock, i);
