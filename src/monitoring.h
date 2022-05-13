eec054926b014e38d472ab7b32fd4e9abdf38b20
HugoFolcher <hugo.folcher@orolia2s.com>
2022-05-13 13:44:44 +0200

    renaming survey_in_error into survey_in_position_error

--- src/monitoring.h
+++ src/monitoring.h
@@ -37,7 +37,7 @@ struct monitoring {
 	int64_t phase_error;
 	int fix;
 	int satellites_count;
-	float survey_in_error;
+	float survey_in_position_error;
 	int leap_seconds;
 	int lsChange;
 	int8_t antenna_power;
