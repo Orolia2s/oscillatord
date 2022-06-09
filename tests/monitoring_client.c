61b6d3f372aad5b54a3393ea5246b6c7b1507806
Charles Parent <charles.parent@orolia2s.com>
2022-06-09 12:05:50 +0200

    Print disciplining parameters in monitoring client

--- tests/monitoring_client.c
+++ tests/monitoring_client.c
@@ -143,6 +143,7 @@ int main(int argc, char *argv[]) {
 	struct json_object *obj = json_send_and_receive(sockfd, request);
 	struct json_object *layer_1;
 	struct json_object *layer_2;
+	struct json_object *layer_3;
 
 	log_info(json_object_to_json_string(obj));
 	
@@ -235,6 +236,58 @@ int main(int argc, char *argv[]) {
 		log_info("\t- survey_in_position_error: %0.2f m", survey_in_position_error);
 		log_info("\t- lsChange: %u", lsChange);
 		log_info("\t- leap_seconds: %u", leap_seconds);
+	}
+
+	/* Disciplining parameters */
+	json_object_object_get_ex(obj, "disciplining_parameters", &layer_1);
+	if (layer_1 != NULL) {
+		log_info("Disciplining parameters detected");
+		json_object_object_get_ex(layer_1, "calibration_parameters", &layer_2);
+		if (layer_2 != NULL) {
+			json_object_object_get_ex(layer_2, "ctrl_nodes_length", &layer_3);
+			int ctrl_nodes_length = json_object_get_int(layer_3);
+			json_object_object_get_ex(layer_2, "ctrl_load_nodes", &layer_3);
+			const char * ctrl_load_nodes = json_object_get_string(layer_3);
+			json_object_object_get_ex(layer_2, "ctrl_drift_coeffs", &layer_3);
+			const char * ctrl_drift_coeffs = json_object_get_string(layer_3);
+			json_object_object_get_ex(layer_2, "coarse_equilibrium", &layer_3);
+			int coarse_equilibrium = json_object_get_int(layer_3);
+			json_object_object_get_ex(layer_2, "calibration_date", &layer_3);
+			int calibration_date = json_object_get_int(layer_3);
+			json_object_object_get_ex(layer_2, "calibration_valid", &layer_3);
+			const char * calibration_valid = json_object_get_string(layer_3);
+			json_object_object_get_ex(layer_2, "ctrl_nodes_length_factory", &layer_3);
+			int ctrl_nodes_length_factory = json_object_get_int(layer_3);
+			json_object_object_get_ex(layer_2, "ctrl_load_nodes_factory", &layer_3);
+			const char * ctrl_load_nodes_factory = json_object_get_string(layer_3);
+			json_object_object_get_ex(layer_2, "ctrl_drift_coeffs_factory", &layer_3);
+			const char * ctrl_drift_coeffs_factory = json_object_get_string(layer_3);
+			json_object_object_get_ex(layer_2, "coarse_equilibrium_factory", &layer_3);
+			int coarse_equilibrium_factory = json_object_get_int(layer_3);
+			json_object_object_get_ex(layer_2, "estimated_equilibrium_ES", &layer_3);
+			int estimated_equilibrium_ES = json_object_get_int(layer_3);
+			log_info("\t- Calibration parameters");
+			log_info("\t\t- ctrl_nodes_length: %d", ctrl_nodes_length);
+			log_info("\t\t- ctrl_load_nodes: %s", ctrl_load_nodes);
+			log_info("\t\t- ctrl_drift_coeffs: %s", ctrl_drift_coeffs);
+			log_info("\t\t- coarse_equilibrium: %d", coarse_equilibrium);
+			log_info("\t\t- calibration_date: %d", calibration_date);
+			log_info("\t\t- calibration_valid: %s", calibration_valid);
+			log_info("\t\t- ctrl_nodes_length_factory: %d", ctrl_nodes_length_factory);
+			log_info("\t\t- ctrl_load_nodes_factory: %s", ctrl_load_nodes_factory);
+			log_info("\t\t- ctrl_drift_coeffs_factory: %s", ctrl_drift_coeffs_factory);
+			log_info("\t\t- coarse_equilibrium_factory: %d", coarse_equilibrium_factory);
+			log_info("\t\t- estimated_equilibrium_ES: %d", estimated_equilibrium_ES);
+		}
+
+		json_object_object_get_ex(layer_1, "temperature_table", &layer_2);
+		if (layer_2 != NULL) {
+			log_info("\t- Temperature table");
+			json_object_object_foreach(layer_2, temperature_range, mean_value) {
+				log_info("\t\t- %s: %s", temperature_range, json_object_get_string(mean_value));
+			}
+		}
+		
 	}
 
 	/* ACTION */
