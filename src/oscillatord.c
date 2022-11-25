4d21f54dcace1503bdc3901927024b522946c813
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-11-25 00:21:51 -0500

    spelling: requested
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/oscillatord.c
+++ src/oscillatord.c
@@ -641,7 +641,7 @@ int main(int argc, char *argv[])
 			monitoring->ctrl_values = ctrl_values;
 			switch(monitoring->request) {
 			case REQUEST_CALIBRATION:
-				log_info("Monitoring: Calibration resquested");
+				log_info("Monitoring: Calibration requested");
 				input.calibration_requested = true;
 				break;
 			case REQUEST_GNSS_START:
