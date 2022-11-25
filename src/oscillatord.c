f5eebeaf5ca54a7feeff3daef5b2ac9f8fe31e1e
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-11-25 00:21:53 -0500

    spelling: writing
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/oscillatord.c
+++ src/oscillatord.c
@@ -431,7 +431,7 @@ int main(int argc, char *argv[])
 				break;
 			}
 			/* Wait for phase error before getting oscillator control values */
-			/* This prevents control values to be read right after writting them */
+			/* This prevents control values to be read right after writing them */
 
 			/* Oscillator control values and temperature are needed for
 			* the disciplining algorithm and monitoring, get both of them
