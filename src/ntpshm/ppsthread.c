0ce760469353743bb74afa75e2f4c836368e2bf0
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-11-25 00:30:41 -0500

    spelling: specific
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/ntpshm/ppsthread.c
+++ src/ntpshm/ppsthread.c
@@ -726,7 +726,7 @@ static void *gpsd_ppsmonitor(void *arg)
     ((volatile struct inner_context_t *)arg)->pps_thread = NULL;
 
     /* before the loop, figure out how we can detect edges:
-     * TIOMCIWAIT, which is linux specifix
+     * TIOMCIWAIT, which is linux specific
      * RFC2783, a.k.a kernel PPS (KPPS)
      * or if KPPS is deficient a combination of the two */
     if (0 > thread_context->devicefd ||
