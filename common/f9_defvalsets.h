ee2c34eed307eca22ff2f624bb54bdfdaecc5795
Matthias Lorentz <matthias.lorentz@orolia.com>
2023-01-05 11:01:23 +0100

    change F9 config fields from 2.20 default cfg file

--- common/f9_defvalsets.h
+++ common/f9_defvalsets.h
@@ -2,7 +2,7 @@
 #define OSCILLATORD_GNSS_F9_DEFVALSETS2
 
 extern const char default_configuration[284][128];
-extern const char default_configuration_v220[284][128];
+extern const char default_configuration_v220[297][128];
 
 
 #endif
