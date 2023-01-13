a5fdb85ee26b4af492c36bbb9c730b2f85178974
Antoine <antoine.gagniere@orolia2s.com>
2023-01-13 16:13:15 +0100

    Remove regex

--- common/utils.h
+++ common/utils.h
@@ -8,26 +8,31 @@
  */
 #ifndef UTILS_H_
 #define UTILS_H_
-#include <dirent.h>
-#include <stddef.h>
-#include <stdbool.h>
-#include <inttypes.h>
 
-#define NS_IN_SECOND 1000000000l
+#include <inttypes.h>
+#include <stdbool.h>
+#include <stddef.h>
+
+#include <dirent.h>
+
+#define NS_IN_SECOND            1000000000l
 #define DUMMY_TEMPERATURE_VALUE -3000.0
 
 #ifndef container_of
-#define container_of(ptr, type, member) ({ \
-	const typeof(((type *)0)->member)*__mptr = (ptr); \
-	(type *)((uintptr_t)__mptr - offsetof(type, member)); })
+# define container_of(ptr, type, member)                        \
+    ({                                                          \
+        const typeof(((type*)0)->member)* __mptr = (ptr);       \
+        (type*)((uintptr_t)__mptr - offsetof(type, member));    \
+    })
 #endif /* ut_container_of */
 
-void file_cleanup(FILE **f);
-void string_cleanup(char **s);
-void fd_cleanup(int *fd);
+void   file_cleanup(FILE** f);
+void   string_cleanup(char** s);
+void   fd_cleanup(int* fd);
 // Formula to compute mRO50 temperature
 double compute_temp(uint32_t reg);
-void find_dev_path(const char *dirname, struct dirent *dir, char *dev_path);
-bool find_file(char * path , char * name, char * file_path);
-int parse_receiver_version(char* textToCheck, int* major, int* minor);
+void   find_dev_path(const char* dirname, struct dirent* dir, char* dev_path);
+bool   find_file(char* path, char* name, char* file_path);
+bool   parse_receiver_version(char* textToCheck, int* major, int* minor);
+
 #endif /* UTILS_H_ */
