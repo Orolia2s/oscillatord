15a149be37dc3a196c4eb40bfe5ffaa32c966982
Antoine <angagnie@student.42.fr>
2023-10-18 16:40:01 +0200

    Remove duplicate function definitions

--- src/common/utils.c
+++ src/common/utils.c
@@ -9,33 +9,6 @@
 
 #include "log.h"
 
-void file_cleanup(FILE **f)
-{
-	if (f == NULL || *f == NULL)
-		return;
-
-	fclose(*f);
-	*f = NULL;
-}
-
-void string_cleanup(char **s)
-{
-	if (s == NULL || *s == NULL)
-		return;
-
-	free(*s);
-	*s = NULL;
-}
-
-void fd_cleanup(int *fd)
-{
-	if (fd == NULL)
-		return;
-
-	close(*fd);
-	*fd = -1;
-}
-
 // Formula to compute mRO50 temperature
 double compute_temp(uint32_t reg)
 {
