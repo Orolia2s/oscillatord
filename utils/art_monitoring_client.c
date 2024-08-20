3bd60f25fbcae3bcc95d69e710c5c67bc519b887
Antoine <antoine.gagniere@orolia2s.com>
2024-08-20 20:46:48 +0200

    Fix addrinfo flags

--- utils/art_monitoring_client.c
+++ utils/art_monitoring_client.c
@@ -8,22 +8,24 @@
  * @copyright Copyright (c) 2022
  *
  */
-#include <arpa/inet.h>
-#include <assert.h>
+#include "log.h"
+#include "monitoring.h"
+
 #include <getopt.h>
 #include <json-c/json.h>
+#include <netdb.h>
+
+#include <arpa/inet.h>
 #include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#include <assert.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/socket.h>
-#include <sys/types.h>
 #include <unistd.h>
-#include <netdb.h>
-#include <errno.h>
-
-#include "log.h"
-#include "monitoring.h"
 
 static void print_help(void)
 {
